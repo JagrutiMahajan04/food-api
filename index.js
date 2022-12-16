5ba95741ba0bc1f733b666a49f9be0ba41ea925e
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-16 15:30:09 +0530

    delete-food-item-by id

--- index.js
+++ index.js
@@ -29,10 +29,14 @@ app.get('/all-food-items', (req, res) => {
 
 // add new item
 app.post('/all-food-items', (req, res) => {
-    const id = req.body.id;
-    const title = req.body.title;
-    const price = req.body.price;
-    const category = req.body.category;
+
+    const { id, title, price, category } = req.body;
+
+    //long method for get,post
+    // const id = req.body.id;
+    // const title = req.body.title;
+    // const price = req.body.price;
+    // const category = req.body.category;
 
     const newItem = {
         id: id,
@@ -40,6 +44,16 @@ app.post('/all-food-items', (req, res) => {
         price: price,
         category: category
     }
+
+    //check if food item already exists the same id
+    db.forEach((item) => {
+        if (item.id === id) {
+            return res.json({
+                success: false,
+                message: 'Food item already exists'
+            })
+        }
+    })
 
     db.push(newItem);
 
@@ -50,6 +64,43 @@ app.post('/all-food-items', (req, res) => {
     })
 })
 
+app.get('/food-item-by-id', (req, res) => {
+
+    //read id from query params
+    const id = req.query.id
+
+    db.forEach((item) => {
+        if (item.id == id) {
+            return res.json({
+                success: true,
+                data: item,
+                message: 'Food item fetched successfully'
+            })
+        }
+    })
+})
+
+app.get('/delete-food-item-by-id',(req,res)=>{
+    const id = req.query.id
+
+    db.forEach((item, index)=>{
+        if(item.id == id){
+            db.splice(index, 1)
+            return res.json({
+                success: true,
+                data: db,
+                message: 'Food item deleted successfully'
+            })
+        }
+    })
+
+    return res.json({
+        success: false,
+        data: null,
+        message: 'Food item not found.'
+    })
+
+})
 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000")
