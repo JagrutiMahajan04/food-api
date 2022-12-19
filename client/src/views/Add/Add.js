307dd67b41fa9901365c1944068e743a96b609af
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-19 17:33:38 +0530

    static views for Show & Add

---
+++ client/src/views/Add/Add.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Add.css"
+
+function Add(){
+    return(
+        <div>
+           <h1>Add Food</h1>
+        </div>
+    )
+}
+
+export default Add
+
+
+
+
+
+
+
+
+
+
