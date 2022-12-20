c855769aad6a45afe38127bffd984000265cad92
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-20 14:56:44 +0530

    add food item

--- client/src/views/Show/Show.js
+++ client/src/views/Show/Show.js
@@ -1,39 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import FoodItemCard from '../../component/FoodItemCard/FoodItemCard'
 import "./Show.css"
 
+
 function Show() {
 
-    const [foodItems, setFoodItems] = useState([
-        {
-            id: 1,
-            title: 'idli',
-            price: 15,
-            category: 'breakfast'
-        },
-        {
-            id: 2,
-            title: 'Dosa',
-            price: 20,
-            category: 'breakfast'
-        },
-        {
-            id: 3,
-            title: 'Biryani',
-            price: 200,
-            category: 'lunch'
-        }
-    ])
+    const [foodItems, setFoodItems] = useState([])
+
+    async function loadData(){
+        //load data from API
+        const response = await axios.get('/all-food-items')
+        setFoodItems(response.data.data)
+    }
+
+    useEffect(()=>{
+       loadData()
+    },[])
+
     return (
         <div>
             <h1>Show All Food Items</h1>
             {
-                foodItems.map((item) => {
+                foodItems.map((item,index) => {
                     return (<FoodItemCard 
                              id={item.id} 
                              title={item.title} 
                              category={item.category}
-                             price={item.price}/>)
+                             price={item.price}
+                             key={index}
+                             />)
                 })
             }
         </div>
